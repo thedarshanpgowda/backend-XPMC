20540e5625f100efe5965786cac75f431efa2c2a
Darshan <sdarshanpgowda2003@gmail.com>
2024-07-18 20:22:55 +0530

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://4.200.24.75:4000/auth/login/', 
+    origin: 'http://4.200.24.75:4000', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
